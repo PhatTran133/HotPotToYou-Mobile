05c34e17e5ade4b44be3e430dec6378ff82354d1
nguyễn khương <146073034+nguyen-khuongca@users.noreply.github.com>
2024-07-19 08:21:48 +0700

    order

--- Mobile/Hot Pot To You/lib/models/order_model.dart
+++ Mobile/Hot Pot To You/lib/models/order_model.dart
@@ -20,17 +20,5 @@ class OrderBuyModel {
     this.paymentName,
   });
 
-  factory OrderBuyModel.fromJson(Map<String, dynamic> json) {
-    return OrderBuyModel(
-      orderBuyID: json['orderId'],
-      total: json['totalPrice'].toDouble(),
-      status: json['status'],
-      paymentName: json['paymentName'],
-      customerName: json['nameCustomer'],
-      customerAddress: json['addressCustomer'],
-      customerPhone: json['phoneCustomer'],      
-      dateOrder: DateTime.parse(json['orderDate']),
-      
-    );
-  }
+  
 }
