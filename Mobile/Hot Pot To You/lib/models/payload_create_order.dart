8be16e6d7efe8b78bd2ed3219d72ef9e9c55f1fc
nguyễn khương <146073034+nguyen-khuongca@users.noreply.github.com>
2024-07-19 08:08:18 +0700

    Update payload

--- Mobile/Hot Pot To You/lib/models/payload_create_order.dart
+++ Mobile/Hot Pot To You/lib/models/payload_create_order.dart
@@ -4,6 +4,11 @@
 
 import 'dart:convert';
 
+PayloadCreateOrder payloadCreateOrderFromJson(String str) =>
+    PayloadCreateOrder.fromJson(json.decode(str));
+
+String payloadCreateOrderToJson(PayloadCreateOrder data) =>
+    json.encode(data.toJson());
 
 class PayloadCreateOrder {
   DateTime? purchaseDate;
@@ -11,7 +16,7 @@ class PayloadCreateOrder {
   String? adress;
   double? totalPrice;
   int? paymentId;
-  
+  List<Item>? items;
 
   PayloadCreateOrder({
     this.purchaseDate,
@@ -19,7 +24,7 @@ class PayloadCreateOrder {
     this.adress,
     this.totalPrice,
     this.paymentId,
-    
+    this.items,
   });
 
   PayloadCreateOrder copyWith({
@@ -28,7 +33,7 @@ class PayloadCreateOrder {
     String? adress,
     double? totalPrice,
     int? paymentId,
-    
+    List<Item>? items,
   }) =>
       PayloadCreateOrder(
         purchaseDate: purchaseDate ?? this.purchaseDate,
@@ -36,7 +41,60 @@ class PayloadCreateOrder {
         adress: adress ?? this.adress,
         totalPrice: totalPrice ?? this.totalPrice,
         paymentId: paymentId ?? this.paymentId,
+        items: items ?? this.items,
+      );
 
+  factory PayloadCreateOrder.fromJson(Map<String, dynamic> json) =>
+      PayloadCreateOrder(
+        purchaseDate: json["purchaseDate"] == null
+            ? null
+            : DateTime.parse(json["purchaseDate"]),
+        customerId: json["customerID"],
+        adress: json["adress"],
+        totalPrice: json["totalPrice"],
+        paymentId: json["paymentID"],
+        
+            
+      );
+
+  Map<String, dynamic> toJson() => {
+        "purchaseDate": purchaseDate?.toIso8601String(),
+        "customerID": customerId,
+        "adress": adress,
+        "totalPrice": totalPrice,
+        "paymentID": paymentId,
+        
+      };
+}
+
+class Item {
+  String? type;
+  int? id;
+  int? quantity;
+  double? total;
+  bool? isPackage;
+
+  Item({
+    this.type,
+    this.id,
+    this.quantity,
+    this.total,
+    this.isPackage,
+  });
+
+  Item copyWith({
+    String? type,
+    int? id,
+    int? quantity,
+    double? total,
+    bool? isPackage,
+  }) =>
+      Item(
+        type: type ?? this.type,
+        id: id ?? this.id,
+        quantity: quantity ?? this.quantity,
+        total: total ?? this.total,
+        isPackage: isPackage ?? this.isPackage,
       );
 
   
